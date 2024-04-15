9754f4226ff9709d6316584c67c97b58b70d9551
sreelakshmigajulapalle@gmail.com <sreelakshmigajulapalle@gmail.com>
2024-04-15 22:57:10 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
+let rock = 'rock';
+let scissors = 'scissors';
+let paper = 'paper';
 
+// these two variables will be changed to test the conditional
+let firstPlayerChoice;
+let secondPlayerChoice;
 
-// Iteration 3: Loops
+let firstPossibility;
+let secondPossibility;
+let thirdPossibility;
+
+let fourthPossibility;
+let fifthPossibility;
+let sixthPossibility;
+
+let seventhPossibility;
+
+firstPossibility = firstPlayerChoice === rock && secondPlayerChoice === scissors;
+
+secondPossibility = firstPlayerChoice === scissors && secondPlayerChoice === paper;
+thirdPossibility = firstPlayerChoice === paper && secondPlayerChoice === rock;
+fourthPossibility = secondPlayerChoice === rock && firstPlayerChoice === scissors;
+fifthPossibility = secondPlayerChoice === scissors && firstPlayerChoice === paper;
+sixthPossibility = secondPlayerChoice === paper && firstPlayerChoice === rock;
+
+seventhPossibility = firstPlayerChoice === secondPlayerChoice;
+
+if (firstPossibility || secondPossibility || thirdPossibility) {
+  console.log(`first player won!`);
+} else if (fourthPossibility || fifthPossibility || sixthPossibility) {
+  console.log(`second player won!`);
+} else if (seventhPossibility) {
+  console.log('there is a tie! pick again');
+} else {
+  console.log('something went wrong!');
+}
