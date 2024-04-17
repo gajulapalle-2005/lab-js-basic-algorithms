0a67a84c90e2f016afda0b44bc1171969e08c261
sreelakshmigajulapalle@gmail.com <sreelakshmigajulapalle@gmail.com>
2024-04-17 21:00:50 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,38 +1,33 @@
 console.log("I'm ready!");
-let rock = 'rock';
-let scissors = 'scissors';
-let paper = 'paper';
+let hacker1= "david";
+console.log("The driver's name is "+ hacker1);
 
-// these two variables will be changed to test the conditional
-let firstPlayerChoice;
-let secondPlayerChoice;
+let hacker2="atul";
+console.log("The navigator's name is " + hacker2);
 
-let firstPossibility;
-let secondPossibility;
-let thirdPossibility;
 
-let fourthPossibility;
-let fifthPossibility;
-let sixthPossibility;
 
-let seventhPossibility;
 
-firstPossibility = firstPlayerChoice === rock && secondPlayerChoice === scissors;
-
-secondPossibility = firstPlayerChoice === scissors && secondPlayerChoice === paper;
-thirdPossibility = firstPlayerChoice === paper && secondPlayerChoice === rock;
-fourthPossibility = secondPlayerChoice === rock && firstPlayerChoice === scissors;
-fifthPossibility = secondPlayerChoice === scissors && firstPlayerChoice === paper;
-sixthPossibility = secondPlayerChoice === paper && firstPlayerChoice === rock;
-
-seventhPossibility = firstPlayerChoice === secondPlayerChoice;
-
-if (firstPossibility || secondPossibility || thirdPossibility) {
-  console.log(`first player won!`);
-} else if (fourthPossibility || fifthPossibility || sixthPossibility) {
-  console.log(`second player won!`);
-} else if (seventhPossibility) {
-  console.log('there is a tie! pick again');
-} else {
-  console.log('something went wrong!');
+if(hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has "+hacker1.length +" characters.");
 }
+else{
+    if(hacker1.length < hacker2.length){
+        console.log("It seems that the navigator has the longest name, it has " + hacker2.length +" characters.");
+    }
+    else
+    {
+        console.log("Wow, you both have equally long names, "+hacker1.length+" characters!");
+    }
+}
+
+
+
+
+console.log(hacker1.toUpperCase() .split("") .join(" "));
+
+let newString = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    newString += hacker2[i];
+}
+console.log(newString);
